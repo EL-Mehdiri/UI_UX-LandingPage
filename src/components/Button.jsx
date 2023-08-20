0dd6f0d51d6326afdbc4fc628e0da2513b4a7915
MehdiRidmani <mahdiridmani22@gmail.com>
2023-08-20 01:01:55 +0100

    done with this components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,11 @@ const Button = ({ label, iconUrl }) => {
     return (
         <button className="flex bg-coral-red font-montserrat border border-coral-red leading-none text-lg rounded-full py-4  justify-center gap-2 px-7 items-center text-white">
             {label}
-            <img src={iconUrl} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
+
+            {iconUrl &&
+                <img src={iconUrl} alt="arrow" className="ml-2 rounded-full w-5 h-5"
+                />
+            }
         </button>
     )
 }
